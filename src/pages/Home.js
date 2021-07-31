a2e2d4bb0c1c38a9fe7f4d26117324b991e4e8d8
Jason Nutt <jnutt367@gmail.com>
2021-07-31 11:27:57 -0400

    added exclamation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { StyledTitle } from '../styled/Random';
 export default function Home() {
     return (
         <div>
-            <StyledTitle>Ready to type?</StyledTitle>
+            <StyledTitle>Ready to type?!</StyledTitle>
             <CTA to="/game">
                 Click or Type '<Accent>s</Accent>' to start playing
             </CTA>
